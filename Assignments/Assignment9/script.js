0a2aa5dd2b0fad439f1e7270dbc25259799b3b3a
jk-nishad-72 <jkn18869@gmail.com>
2026-07-07 05:25:44 +0530

    Ass9 weather and dateTime feature is done

--- Assignments/Assignment9/script.js
+++ Assignments/Assignment9/script.js
@@ -1,22 +1,23 @@
 
 
+//* page Open And Close Functionality
 
+const pageOpenAndCloseFunctionality = function(){
 
-
+    
 const alllElementSection = document.querySelector('.allElems')
-
 
 const  fullElementOFFeaturBox  = document.querySelectorAll('.fullElem')
 
-
 const featureBox  = document.querySelectorAll('.feature')
-
 
 const backBtn = document.querySelectorAll('.back')
 
 const allFeatursVideo = document.querySelectorAll('.feature video')
 
-console.log(allFeatursVideo);
+const h3Tag = document.querySelectorAll('.feature h3')
+
+
 
 
 featureBox.forEach((elem)=>{
@@ -32,11 +33,13 @@ featureBox.forEach((elem)=>{
 elem.addEventListener('mouseenter',()=>{
     // console.log(allFeatursVideo[elem.id]);
     allFeatursVideo[elem.id].style.display = 'flex'
+    h3Tag[elem.id].style.display = 'flex'
 })
 
 elem.addEventListener('mouseleave',()=>{
     // console.log(allFeatursVideo[elem.id]);
     allFeatursVideo[elem.id].style.display = 'none'
+     h3Tag[elem.id].style.display = 'none'
 })
 
 
@@ -58,4 +61,150 @@ backBtn.forEach((elem)=>{
 
 })
 
+}
 
+pageOpenAndCloseFunctionality()
+
+/*
+ *weather and date time 
+ 
+Is task ko 3 parts mein todo
+Part 1: User ka location (lat/lon) nikaalna
+Part 2: Lat/lon se City name nikaalna
+Part 3: Lat/lon se Weather nikaalna
+
+ */
+
+const weatherAndDateTimeFunctionality = function(){
+
+//*Elements selection 
+
+//Date and time elements
+
+const dateTag = document.querySelector('.DateAndTime .date')
+const dayTag = document.querySelector('.DateAndTime .day')
+const timeTag = document.querySelector('.DateAndTime .time')
+const cityTag = document.querySelector('.DateAndTime .city')
+
+//weather elements
+const tempTag = document.querySelector('.temp')
+const weatherCodeTag = document.querySelector('.weatherCode')
+const windDirectionTag = document.querySelector('.windDirection')
+const windSpeedTag = document.querySelector('.windSpeed')
+
+
+
+let langtitude = 0;
+let logtitude = 0;
+
+
+navigator.geolocation.getCurrentPosition(
+(position)=>{
+
+    //Part 1: User ka location (lat/lon) nikaalna
+   langtitude = position.coords.latitude;
+   logtitude = position.coords.longitude;
+
+     getLocation(langtitude , logtitude);
+     getWeatherByLocation(langtitude , logtitude)
+     
+},
+(error)=>{
+
+    alert('Location access denied ❌')
+}
+)
+
+// *Part 2: Lat/lon se City name nikaalna
+async function getLocation(lat, lon) {
+
+
+    // console.log(lat , lon);
+
+  
+
+    const resOfApi = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
+   
+    const jsonData = await resOfApi.json();
+
+
+    // state and country code 
+
+    // console.log(jsonData.address.state , jsonData.address.country_code);
+
+    const city = jsonData.address.state
+    const countryCode =  jsonData.address.country_code;
+    // console.log(city , countryCode);
+
+    
+    cityTag.innerHTML = `${city} <span>(${countryCode}) </span>`
+
+}
+
+//* Part 3: Lat/lon se Weather nikaalna
+async function getWeatherByLocation(lat , lon ) {
+
+          
+
+              const resOfApi =  await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`)
+
+              const data =  await resOfApi.json()
+
+        
+    //   console.log( tempTag ,weatherCodeTag  , windDirectionTag , windSpeedTag);
+    //   console.log(data.current_weather , data.current_weather_units);
+            
+      tempTag.textContent = ` ${data.current_weather.temperature} ${data.current_weather_units.temperature} `
+      weatherCodeTag.textContent = `weather Code: ${data.current_weather.weathercode}  ${data.current_weather_units.weathercode}  `
+      windDirectionTag.textContent = `wind Direction: ${data.current_weather.winddirection}  ${data.current_weather_units.winddirection}  `
+      windSpeedTag.textContent = `wind Speed: ${data.current_weather.windspeed}  ${data.current_weather_units.windspeed}  `
+}
+
+
+// *Date & Time 
+
+const dateAndTimeFun = function(){
+
+     const allDaysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+     const allMonthOfYear = [ "January", "February", "March", "April", "May", "June",
+                             "July", "August", "September", "October", "November", "December" ];
+
+
+    let today = new Date()
+
+    let second = today.getSeconds();
+    let minute = today.getMinutes();
+    let hour = today.getHours() ;
+
+    let dayOfWeek = today.getDay()
+    let monthOfYeer = today.getMonth() 
+
+    let year = today.getFullYear()
+    let tarikh = today.getDate()  
+      
+    let timePeriod = 'AM'
+
+    if(hour > 12 ){
+        hour = hour - 12 
+        timePeriod = 'PM'
+    }
+
+
+   dateTag.textContent = `${tarikh} ${allMonthOfYear[monthOfYeer]} ${year}`
+   dayTag.textContent = ` ${allDaysOfWeek[dayOfWeek]}, ${String(hour).padStart('2','0')}:${String(minute).padStart('2','0')}:${String(second).padStart('2','0')} `
+   timeTag.textContent = ` ${timePeriod}`
+    
+
+}
+
+setInterval(()=>{
+dateAndTimeFun();
+},1000)
+
+
+
+}
+
+
+weatherAndDateTimeFunctionality()
