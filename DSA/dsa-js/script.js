cd4595b944c2d46b2f78bb9b9ba3546eea7b8e8f
JK NIshad <jkn18869@gmail.com>
2026-08-15 22:18:43 +0530

    faksla

--- DSA/dsa-js/script.js
+++ DSA/dsa-js/script.js
@@ -1,31 +1,46 @@
 /**
  * @param {number[]} nums
- * @return {number}
+ * @return {boolean}
  */
-var distinctAverages = function(nums) {
+var checkPrimeFrequency = function(nums) {
+    
+    let map = new Map();
 
-    let distinct = new Set();
+    for( let n of nums){
 
-    while(nums.length > 0){
-        let min = Math.min(...nums);
-        let max = Math.max(...nums);
-        let avg = (min + max) / 2;
-       
-        distinct.add(avg);
-        
-        let minIndex = nums.indexOf(min);
-      nums.splice(minIndex , 1);
-
-        let maxIndex = nums.indexOf(max);
-      nums.splice(maxIndex , 1);
-     
-      
+      map.set(n , (map.get(n) || 0) + 1)
     }
 
-    
-    return distinct.size
-}; 
+    //  console.log(map);
 
-console.log(distinctAverages([4,1,4,0,3,5]));
-console.log(distinctAverages([1,100]));
+     for(let [key , freq] of map){
+
+               
+       if(checkPrime(freq)) return true
+
+     }
+     
+
+     return false
+};
+
+var checkPrime = (num)=>{
+
+    if(num <= 1) return false;
+    if(num === 2 ) return true;
+    if(num %2 === 0)return false
+     for(let i = 3 ; i <= Math.floor(Math.sqrt(num)) ;i += 2){
+
+        if(num % i === 0){
+          return false
+        }
+     }
+    return true
+}
+
+
+console.log(checkPrimeFrequency([1,2,3,4,5,4]));
+console.log(checkPrimeFrequency([1,2,3,4,5]));
+console.log(checkPrimeFrequency([3,0,3,6,3,3]));
+
 
