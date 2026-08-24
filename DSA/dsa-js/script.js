1d0fe6532adbfb4908be10cd903db2239fb4aaae
JK NIshad <jkn18869@gmail.com>
2026-08-24 23:09:51 +0530

    solv something

--- DSA/dsa-js/script.js
+++ DSA/dsa-js/script.js
@@ -1,25 +1,31 @@
 /**
- * @param {string[]} words
- * @return {string[]}
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
  */
-var stringMatching = function(words) {
-
-    let result =[];
-    for(let i=0;i<words.length;i++){
-        for(let j=0;j<words.length;j++){
-            if(i!=j && words[j].includes(words[i])){
-                result.push(words[i]);
-                break;
-            }
-        }
-    }
-    return result; 
-  
+var sumDivisibleByK = function(nums, k) {
     
+    let map = new Map();
+    
+    for(let n of nums ){
+
+        map.set(n , (map.get(n)||0 ) +1 );
+        
+    }
+
+    let sum = 0;
+
+    for(let [key , freq] of map){
+
+          if(freq % k === 0){
+            sum += key * freq
+          }
+    }
+
+    return sum
 };
-console.log(stringMatching(["mass","as","hero","superhero"]));
 
-console.log(stringMatching(["leetcode","et","code"]));
-
-
+console.log(sumDivisibleByK([1,2,3,4,5], k = 2));
+console.log(sumDivisibleByK([1,2,2,3,3,3,3,4], k = 2));
+console.log(sumDivisibleByK([4,4,4,1,2,3], k = 3));
 
