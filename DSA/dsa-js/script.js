80e533559577362dc2a0308b6cf721f679b2af65
JK NIshad <jkn18869@gmail.com>
2026-08-29 23:51:30 +0530

    happy rakshabandan

--- DSA/dsa-js/script.js
+++ DSA/dsa-js/script.js
@@ -1,31 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var sumDivisibleByK = function(nums, k) {
-    
-    let map = new Map();
-    
-    for(let n of nums ){
 
-        map.set(n , (map.get(n)||0 ) +1 );
-        
-    }
-
-    let sum = 0;
-
-    for(let [key , freq] of map){
-
-          if(freq % k === 0){
-            sum += key * freq
-          }
-    }
-
-    return sum
-};
-
-console.log(sumDivisibleByK([1,2,3,4,5], k = 2));
-console.log(sumDivisibleByK([1,2,2,3,3,3,3,4], k = 2));
-console.log(sumDivisibleByK([4,4,4,1,2,3], k = 3));
+console.log('Happy rakshabadan ');
 
