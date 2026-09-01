841cb480edeaaf1c69b52e4567c3c0223d3963af
JK NIshad <jkn18869@gmail.com>
2026-09-01 23:47:00 +0530

    solve something

--- DSA/dsa-js/script.js
+++ DSA/dsa-js/script.js
@@ -1,4 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */ 
 
-console.log('leed code 365 day celelebration ');
+ var reverse = function(arr, i , j){
+
+     while(i<j){
+          let temp = arr[i];
+          arr[i]  = arr[j];
+          arr[j] = temp;
+          i++
+          j--
+     }
+ }
+
+var rotate = function(nums, k) {  
+
+    k = k % nums.length;
+    reverse(nums, 0 , nums.length-1);
+    reverse(nums, k , nums.length-1);
+    reverse(nums, 0 ,k-1 );
+
+    return nums
+};
 
 
+console.log(rotate(nums = [1,2,3,4,5,6,7], k = 3));
